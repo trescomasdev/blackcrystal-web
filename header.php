3bdf2872d1a201ca488917b8e4ebb7c8ed3cf876
Tres Comas <trescomas@icloud.com>
2018-05-08 14:16:07 -0500

    update

--- header.php
+++ header.php
@@ -112,8 +112,9 @@
 									</ul>
 								</div>
 								<div class="header-info mobile">
-									<em><a id="callback-btn-mobile"><i class="fa fa-phone"></i><?php _e('Kérjen Visszahívást', 'theme-phrases')?></a></em>
-
+									<?php if (get_locale() == 'hu_HU'):?>
+										<em><a id="callback-btn-mobile"><i class="fa fa-phone"></i><?php _e('Kérjen Visszahívást', 'theme-phrases')?></a></em>
+									<?php endif;?>									
 								</div>
 							</div>
 						</div>
